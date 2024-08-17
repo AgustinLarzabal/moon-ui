33064bd79a39f06b444ad8b4382a90cbc2433eee
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-08-17 16:27:31 +0200

    add font and basic style config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Moon UI",
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${GeistMono.variable} dark`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
