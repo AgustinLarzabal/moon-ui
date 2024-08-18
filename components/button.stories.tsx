581685a48b3196e83880c1833f7b8e4cb7dbd896
Agustín Larzábal <agustinlarzabal@gmail.com>
2024-08-18 11:55:06 +0200

    add button component

---
+++ components/button.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+
+const meta: Meta<typeof Button> = {
+  title: "Button",
+  component: Button,
+  args: {
+    children: "Button",
+    disabled: false,
+  },
+  argTypes: {
+    asChild: { control: false },
+    size: { control: "select", options: ["default", "sm", "lg", "icon"] },
+    style: { control: false },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ fontFamily: "var(--font-sans)" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {};
