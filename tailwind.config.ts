41f3b02689984076a5e4700ee85b3224c35f0e37
Agustin Larzabal <agustin.larzabal@globant.com>
2024-11-12 14:45:56 +0000

    add safeList to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  safelist: ["dark", "light"],
   theme: {
     extend: {
       colors: {
